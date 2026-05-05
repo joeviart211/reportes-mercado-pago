983a9aa889c06f52b7b7dbcbae8c279dd5453984
Joe Viart <viart211@gmail.com>
2026-05-05 17:31:07 -0600

    remove unique

---
+++ database/migrations/2026_05_05_232948_remove_unique_and_index_from_mp_transactions.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('mp_transactions', function (Blueprint $table) {
+            $table->dropUnique(['branch_id', 'operation_id', 'operation_type']);
+            $table->dropIndex(['operation_id']);
+        });
+}
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('mp_transactions', function (Blueprint $table) {
+            $table->unique(['branch_id', 'operation_id', 'operation_type']);
+            $table->index('operation_id');
+        });
+    }
+};
