b69922e83ea00292ad9149d1bfb1228fdcd58e8c
Joe Viart <viart211@gmail.com>
2026-05-06 12:03:52 -0600

    cambios migracion

--- database/migrations/2026_05_05_232948_remove_unique_and_index_from_mp_transactions.php
+++ database/migrations/2026_05_05_232948_remove_unique_and_index_from_mp_transactions.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::table('mp_transactions', function (Blueprint $table) {
             $table->dropUnique([ 'operation_id', 'operation_type']);
-            $table->dropIndex(['operation_id']);
         });
 }
 
@@ -24,7 +23,6 @@ return new class extends Migration
     {
         Schema::table('mp_transactions', function (Blueprint $table) {
             $table->unique(['operation_id', 'operation_type']);
-            $table->index('operation_id');
         });
     }
 };
